13b4a4ba0d09eae48768debfce6ef1a6100854ae
jcaluiz <lacjunior129.la@gmail.com>
2023-03-18 13:02:28 -0300

    Fazendo a configuração do CORS

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: python swapi/main.py
 release: python swapi/db.py
-web: sh build.sh && uvicorn swapi.main:app --host 0.0.0.0 --port $PORT
+web: sh build.sh && uvicorn swapi.main:app --host 0.0.0.0 --port $PORT workers 1
--- swapi/main.py
+++ swapi/main.py
@@ -4,12 +4,27 @@ from sqlmodel import Session, select
 from swapi.db_populate import populate_empty_tables
 from swapi.model import Planet
 import os
+from fastapi.middleware.cors import CORSMiddleware
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "data", "database.sqlite")
 
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://example.com",
+    "https://www.example.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_session():
     return Session(engine)
